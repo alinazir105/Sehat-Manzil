0a946e4edc55b56f4d66ca129750f0cac549da2c
alinazir105 <alinazir105@gmail.com>
2024-10-11 00:15:28 +0500

    on boarding screens done

--- Sehat-Manzil-frontend/app/index.jsx
+++ Sehat-Manzil-frontend/app/index.jsx
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import SplashScreen from './SplashScreen'; // Import the new SplashScreen
-
+import OnBoarding from './OnBoarding';
 export default function App() {
   return (
     <>
-      <SplashScreen />
+     <OnBoarding />
       <StatusBar backgroundColor="#161622" style="dark" />
     </>
   );
